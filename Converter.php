b4c8aab752531186b5075a328397870a19a8b816
dishamistry <mistrydisha6@gmail.com>
2022-11-14 14:21:12 +1100

    First Commit

---
+++ Converter.php
@@ -0,0 +1,11 @@
+<?php
+
+class Converter
+{
+    public static function convertCurrency($amount = 1, $from = 'AUD', $to = 'NZD')
+    {
+        try {
+        } catch (\Exception $e) {
+        }
+    }
+}
